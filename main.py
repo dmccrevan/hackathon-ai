21d64d998e7a38236c1ec50952d8176aedcad230
Steven vanZyl <rushsteve1@rushsteve1.us>
2019-11-02 18:07:46 -0400

    Pastebin!
    I created a simple pastebin feature because I could.
    And why not.
    
    Also merged in the actual generator into the frontend and
    fixed some other issues.

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, redirect, request
+from scripts.generate import Generator
+
+mygen = Generator()
+
 app = Flask(__name__)
+
+PASTES_DIR='/tmp/hackathin-pastebin' # tempfile.mkdtemp()
+try:
+    os.mkdir(PASTES_DIR)
+except:
+    pass
 
 @app.route('/')
 def index():
@@ -7,4 +18,26 @@ def index():
 
 @app.route('/idea')
 def idea():
-    return render_template('idea.html', description="A project description")
+    return render_template('idea.html', tagline=mygen.generate_tagline())
+
+@app.route('/pastes-list')
+def pastes_list():
+    return render_template('pastes-list.html', pastes=os.listdir(PASTES_DIR))
+
+@app.route('/new-paste', methods=['GET', 'POST'])
+def new_paste():
+    if request.method == 'POST':
+    	name  = request.form['name']
+    	content = request.form['content']
+    	with open(PASTES_DIR + '/' + name, 'w+') as f:
+            f.write(content)
+    	return redirect('/paste/' + str(name))
+    else:
+    	return render_template('new-paste.html')
+
+@app.route('/paste/<name>')
+def paste(name=None):
+    if name is None:
+        return redirect('/pastes-list')
+    with open(PASTES_DIR + '/' + name, 'r') as f:
+        return render_template('paste.html', name=name, content=f.read())
