39ccb2979be247a0abd5bbc10e927dcff5dd980d
Josh Lebedinsky <joshlebed@gmail.com>
2019-11-02 15:00:30 -0400

    added pickling

--- parser/html-parser.py
+++
@@ -1,6 +0,0 @@
-import urllib2
-response = urllib2.urlopen(url)
-html = response.read()
-url = "https://devpost.com/software/search?page=1&query=has%3Aimage"
-
-# print(urllib.request.urlopen(url).read())
