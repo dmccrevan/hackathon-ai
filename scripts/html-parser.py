1fa382091c2de42acddeade7989247bc044b7856
Josh Lebedinsky <joshlebed@gmail.com>
2019-11-02 15:34:59 -0400

    add parser

--- scripts/html-parser.py
+++ scripts/html-parser.py
@@ -1,6 +1,31 @@
-url = 'https://devpost.com/software/search?page=1'
 import urllib.request
-with urllib.request.urlopen(url) as response:
-  html = response.read()
+from bs4 import BeautifulSoup
+import pickle
 
-soup = BeautifulSoup(html, 'html.parser')
+url = 'https://devpost.com/software/popular?page='
+max_page = 55
+# max_page = 5511
+data = []
+
+for page_num in range(max_page):
+
+  print(f'looking at page {page_num} of {max_page}')
+
+  with urllib.request.urlopen(url + str(page_num)) as response:
+    html = response.read()
+
+  soup = BeautifulSoup(html, 'html.parser')
+  entry_divs = soup.findAll("div", {"class": "software-entry-name"})
+
+  for div in entry_divs:
+    title = div.find('h5').contents[0].strip()
+    tagline = div.findAll("p", {"class": "small tagline"})[0].contents[0].strip()
+    data.append((title, tagline))
+
+    print(title)
+    print(tagline)
+    print()
+
+pickle.dump(data, open('data.pickle', 'wb'))
+
+# pickle.load(open("data.pickle","rb"))
