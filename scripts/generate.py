040f8d23e1a326d1db9771e5da7ccfa4d2d12ebe
Josh Lebedinsky <joshlebed@gmail.com>
2019-11-02 23:19:56 -0400

    work on lstm

--- scripts/generate.py
+++ scripts/generate.py
@@ -7,24 +7,29 @@ class Generator:
   def __init__(self):
     self.BEGIN = '__BEGIN__'
     self.END = '__END__'
-    filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model.pickle')
-    file = open(filepath, "rb")
-    self.model = pickle.load(file)
+    # file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+      # 'tagline-lstm-model.pickle'), "rb")
+    # self.tagline_lstm_model = pickle.load(file)
+    file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+      'tagline-markov-model.pickle'), "rb")
+    self.tagline_markov_model = pickle.load(file)
 
 
-  # TODO: try to get consistent lengths - steer away from titles ending super quickly, steer towards line ends when we get too long
+  # TODO: try to get consistent lengths - steer away from titles ending super \
+  # quickly, steer towards line ends when we get too long
   # should cut off titles that are too long at some point
 
   def find_next_word(self, prev_word):
-    if not prev_word in self.model or len(self.model[prev_word]) < 1:
+    if not prev_word in self.tagline_markov_model or
+    len(self.tagline_markov_model[prev_word]) < 1:
       return END
 
     targets = []
     probabilities = []
 
-    for target in self.model[prev_word]:
+    for target in self.tagline_markov_model[prev_word]:
       targets.append(target)
-      probabilities.append(self.model[prev_word][target])
+      probabilities.append(self.tagline_markov_model[prev_word][target])
 
     draw = choice(targets, 1, p=probabilities)[0]
     return draw
